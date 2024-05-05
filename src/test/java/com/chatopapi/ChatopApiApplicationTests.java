8fe053ec10d8c7bdcd8b5d60daa9e8ed1455ab5a
Marconnet Francois <marconnetf@live.fr>
2024-05-05 11:23:20 +0200

    first commit

---
+++ src/test/java/com/chatopapi/ChatopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
